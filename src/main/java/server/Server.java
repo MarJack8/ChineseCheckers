193a05097c987a8a8f7128290892dd03e02787a8
Tomasz <cansisti@outlook.com>
2018-12-27 20:32:11 +0100

    "not permitted" fix

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -23,65 +23,68 @@ public class Server {
 			// In this point already server sent "start_success" to all players
 			Board board = new Board( gm.getPlayerCount() );
 			for( int currentPlayer = (new Random()).nextInt( gm.getPlayerCount() ); !gm.gameFinished(); currentPlayer = ( currentPlayer + 1 ) % gm.getPlayerCount() ) {
-				if( gm.getWin( currentPlayer ) ) 
-				gm.permitPlayer( currentPlayer );
-				gm.sendToPlayer( currentPlayer, new CCMessage( "your_turn" ) );
-				
-				Field selected = null;
-				ArrayList<Field> legal = null;
-				
-				while( true ) {
-					CCMessage pm = gm.recvFromPlayer( currentPlayer );
-					if( pm.getSignal().equals( "pass" ) ) {
-						gm.sendToPlayer( currentPlayer, new CCMessage( "success" ) );
-						System.out.println( "#" + currentPlayer + " passed" );
-						break;
-					}
-					else if( pm.getSignal().equals( "select" ) ) {
-						ArrayList<Integer> ag = pm.getArgs();
-						selected = board.getNode( ag.get( 0 ), ag.get( 1 ) );
-						if( selected.getFieldColor() == FieldColor.values()[ currentPlayer + 1 ] ) {
-							legal = board.getLegal( selected );
-							CCMessage sm = new CCMessage( "legal" );
-							for( Field f: legal ) {
-								sm.insertArg( f.getYCord() );
-								sm.insertArg( f.getXCord() );
-							}
-							gm.sendToPlayer( currentPlayer, sm );
+				if( !gm.getWin( currentPlayer ) ) {
+					gm.permitPlayer( currentPlayer );
+					gm.sendToPlayer( currentPlayer, new CCMessage( "your_turn" ) );
+					
+					System.out.println( "#" + currentPlayer + "'s turn" );
+					
+					Field selected = null;
+					ArrayList<Field> legal = null;
+					
+					while( true ) {
+						CCMessage pm = gm.recvFromPlayer( currentPlayer );
+						if( pm.getSignal().equals( "pass" ) ) {
+							gm.sendToPlayer( currentPlayer, new CCMessage( "success" ) );
+							System.out.println( "#" + currentPlayer + " passed" );
+							break;
 						}
-						else {
+						else if( pm.getSignal().equals( "select" ) ) {
+							ArrayList<Integer> ag = pm.getArgs();
+							selected = board.getNode( ag.get( 0 ), ag.get( 1 ) );
+							if( selected.getFieldColor() == FieldColor.values()[ currentPlayer + 1 ] ) {
+								legal = board.getLegal( selected );
+								CCMessage sm = new CCMessage( "legal" );
+								for( Field f: legal ) {
+									sm.insertArg( f.getYCord() );
+									sm.insertArg( f.getXCord() );
+								}
+								gm.sendToPlayer( currentPlayer, sm );
+							}
+							else {
+								gm.sendToPlayer( currentPlayer, new CCMessage( "illegal" ) );
+							}
+						}
+						else if( pm.getSignal().equals( "move" ) ) {
+							if( selected != null && legal != null ) {
+								ArrayList<Integer> ag = pm.getArgs();
+								Field destination = board.getNode( ag.get( 0 ), ag.get( 1 ) );
+								if( legal.contains( destination ) ) {
+									gm.sendToPlayer( currentPlayer, new CCMessage( "success" ) );
+									CCMessage sm = new CCMessage( "move" );
+									sm.insertArg( selected.getYCord() );
+									sm.insertArg( selected.getXCord() );
+									sm.insertArg( destination.getYCord() );
+									sm.insertArg( destination.getXCord() );
+									gm.sendToAll( sm );
+									destination.setColor( FieldColor.values()[ currentPlayer + 1 ] );
+									selected.setColor( FieldColor.NO_PLAYER );
+									break;
+								}
+							}
 							gm.sendToPlayer( currentPlayer, new CCMessage( "illegal" ) );
 						}
-					}
-					else if( pm.getSignal().equals( "move" ) ) {
-						if( selected != null && legal != null ) {
-							ArrayList<Integer> ag = pm.getArgs();
-							Field destination = board.getNode( ag.get( 0 ), ag.get( 1 ) );
-							if( legal.contains( destination ) ) {
-								gm.sendToPlayer( currentPlayer, new CCMessage( "success" ) );
-								CCMessage sm = new CCMessage( "move" );
-								sm.insertArg( selected.getYCord() );
-								sm.insertArg( selected.getXCord() );
-								sm.insertArg( destination.getYCord() );
-								sm.insertArg( destination.getXCord() );
-								gm.sendToAll( sm );
-								destination.setColor( FieldColor.values()[ currentPlayer + 1 ] );
-								selected.setColor( FieldColor.NO_PLAYER );
-								break;
-							}
+						else {
+							// Bad usage on server side
+							gm.sendToPlayer( currentPlayer, new CCMessage( "no?" ) );
 						}
-						gm.sendToPlayer( currentPlayer, new CCMessage( "illegal" ) );
 					}
-					else {
-						// Bad usage on server side
-						gm.sendToPlayer( currentPlayer, new CCMessage( "no?" ) );
+					gm.haltPlayer( currentPlayer );
+					
+					if( false /*currentPlayer has won*/ ) {
+						gm.sendToPlayer( currentPlayer, new CCMessage( "victory" ) );
+						gm.setWin( currentPlayer );
 					}
-				}
-				gm.haltPlayer( currentPlayer );
-				
-				if( false /*currentPlayer has won*/ ) {
-					gm.sendToPlayer( currentPlayer, new CCMessage( "victory" ) );
-					gm.setWin( currentPlayer );
 				}
 			}
 			gm.sendToAll( new CCMessage( "finished" ) );
